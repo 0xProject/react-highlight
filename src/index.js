1813ab5bd2550eb94ad9c284ca52fc5f4c5ab5ca
Fabio Berger <me@fabioberger.com>
2018-02-17 19:54:47 -0700

    Add Solidity syntax highlighting support

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-import hljs from 'highlight.js';
-import React from 'react';
-import ReactDOM from 'react-dom';
+import hljs from "highlight.js";
+import hljsDefineSolidity from "highlightjs-solidity";
+import React from "react";
+import ReactDOM from "react-dom";
+
+hljsDefineSolidity(hljs);
+hljs.initHighlightingOnLoad();
 
 class Highlight extends React.Component {
     componentDidMount() {
@@ -13,7 +17,7 @@ class Highlight extends React.Component {
 
     highlightCode() {
         const domNode = ReactDOM.findDOMNode(this);
-        const nodes = domNode.querySelectorAll('pre code');
+        const nodes = domNode.querySelectorAll("pre code");
 
         let i;
         for (i = 0; i < nodes.length; i++) {
@@ -22,20 +26,30 @@ class Highlight extends React.Component {
     }
 
     render() {
-        const {children, className, element, innerHTML} = this.props;
+        const { children, className, element, innerHTML } = this.props;
         let Element = element ? React.DOM[element] : null;
 
         if (innerHTML) {
             if (!Element) {
-                Element = React.DOM.div
+                Element = React.DOM.div;
             }
 
-            return Element({dangerouslySetInnerHTML: {__html: children}, className: className || null}, null);
+            return Element(
+                {
+                    dangerouslySetInnerHTML: { __html: children },
+                    className: className || null
+                },
+                null
+            );
         } else {
             if (Element) {
-                return Element({className}, children);
+                return Element({ className }, children);
             } else {
-                return <pre><code className={className}>{children}</code></pre>;
+                return (
+                    <pre>
+                        <code className={className}>{children}</code>
+                    </pre>
+                );
             }
         }
     }
@@ -44,7 +58,7 @@ class Highlight extends React.Component {
 Highlight.defaultProps = {
     innerHTML: false,
     className: null,
-    element: null,
+    element: null
 };
 
 export default Highlight;
